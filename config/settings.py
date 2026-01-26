41396d18a9e5923f134e5b7e0876c8b869149cfa
Maxime Desmet Vanden Stock <maxime.desmet@breev.eu>
2026-01-26 15:56:00 +0100

    PR suggestions

--- config/settings.py
+++ config/settings.py
@@ -73,6 +73,7 @@ if DEBUG:
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django_htmx.middleware.HtmxMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -161,6 +162,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # django-cotton components directory
